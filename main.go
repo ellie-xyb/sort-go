55a5840e0839ca1b81087234795525907ebdef1f
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-05 03:45:49 +0900

    default sort func in go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+func getData() []int {
+	data := []int{}
+	for i := 0; i < 10000000; i++ {
+		data = append(data, rand.Intn(4294998))
+	}
+	return data
+}
 
 func main() {
-	fmt.Println("hello word")
+	// fmt.Println("hello word")
+	data := getData()
+	sort.Ints(data)
+	fmt.Println(data[0])
 }
